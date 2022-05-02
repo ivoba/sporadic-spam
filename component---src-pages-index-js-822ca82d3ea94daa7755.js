d0134db5b724feb33f3b68b7250a4b23ab154910
Ivo Bathke <ivo.bathke@gmail.com>
2022-05-02 09:58:39 +0200

    Updates

--- component---src-pages-index-js-822ca82d3ea94daa7755.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{RXBc:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),l=n.n(a),i=n("Bl7J"),r=n("vrFN"),c=n("ftA+"),u=n("pCl6"),s=n.n(u),o=n("RMg3");t.default=function(){return l.a.createElement(i.a,null,l.a.createElement(r.a,{title:"Willkommen",keywords:["ecotainment","newsletter"],description:"Umwelt, Wissenschaft, Aktivisten, Birding, Subkultur, Trivia"}),l.a.createElement(s.a,null,l.a.createElement(o.a,null),l.a.createElement(c.a,null)))}}}]);
-//# sourceMappingURL=component---src-pages-index-js-822ca82d3ea94daa7755.js.map
